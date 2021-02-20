499f12faf8abc68652bbe5a76975709cf80c0640
Bromaster369 <chrispardopersonal@gmail.com>
2021-02-20 17:03:19 -0500

    added paths into application/layout

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,12 @@ Rails.application.routes.draw do
   delete '/logout' => 'sessions#destroy'
   
   resources :comments
-  resources :users
-  resources :experiences
+  resources :users do 
+    resources :experiences, only: [:new, :create, :index]
+  end
+  resources :experiences do 
+    resources :comments, only: [:new, :create, :index]
+  end 
   resources :categories
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
