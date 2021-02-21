8378b85567c13b4678b825b7f6a19daaa975fb6b
Bromaster369 <chrispardopersonal@gmail.com>
2021-02-21 12:12:54 -0500

    created and arranged nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,14 +13,15 @@ Rails.application.routes.draw do
   #logout route
   delete '/logout' => 'sessions#destroy'
   
+  resources :experiences do 
+    resources :comments
+  end 
   resources :comments
   resources :users do 
-    resources :experiences, only: [:new, :create, :index]
+    resources :experiences, shallow: true
   end
-  resources :experiences do 
-    resources :comments, only: [:new, :create, :index]
-  end 
-  resources :categories
+  
+  resources :categories, only: [:index, :show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
