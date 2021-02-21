9d348911854117c2e3158829a5665f9dd1d8a2b5
Bromaster369 <chrispardopersonal@gmail.com>
2021-02-21 12:37:18 -0500

    set index and new comment view. Actions for comment controller were set except update and comment_params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,46 @@
 class CommentsController < ApplicationController
 
+    before_action :redirect_if_not_logged_in
 
-    
+    def index
+        if params[:experience_id] && @experience = Experience.find_by_id(params[:experience_id])
+            @comments = @experience.comments
+         else
+           @error = "That experience doesn't exist" if params[:experience_id]
+           @comments = Comment.all
+         end 
+    end 
 
+    def new 
+        if params[:post_id] && @experience = Experience.find_by_id(params[:experience_id])
+            @comment = @experience.comments.build
+          else
+            @error = "That experience doesn't exist" if params[:experience_id]
+            @comment = Comment.new
+        end
+    end 
+
+    def create 
+        @comment = current_user.comments.build(comment_params)
+        if @comment.save
+            redirect_to comments_path
+        else
+            render :new
+        end
+    end 
+
+    def show 
+    end 
+
+    def edit 
+    end
+
+    def update
+    end 
+
+    private 
+
+    def comment_params
+    end 
 
 end
