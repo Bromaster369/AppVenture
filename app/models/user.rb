498bdc23c95cc5278d7fff98b1f0e336a88af4b9
Bromaster369 <chrispardopersonal@gmail.com>
2021-02-25 09:12:34 -0500

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
     has_many :experiences
     has_many :comments
     has_many :commented_experiences, through: :comments, source: :experience
+    has_many :categories, through: :experiences
+
+    validates :username, :email, presence: true
+
 end
